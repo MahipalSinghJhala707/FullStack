ec326d3267260615ebb708b2a848b84943a2e02b
mahipal <mahipalsinghjhala707@gmail.com>
2024-08-23 13:20:42 +0530

    Day10

---
+++ Day10/class.js
@@ -0,0 +1,13 @@
+// note this is destructuring the array
+// const arr = [1,2,3,[4,[5,6]]];
+// const [a,b,c,[d,[e,f]]] = arr;
+
+// console.log(a,b,c,d,e,f);
+
+// note this is destructuring of the object
+
+
+
+
+
+
