caf4cb8f5dccd1a76400d88d3e3e866579718995
mahipal <mahipalsinghjhala707@gmail.com>
2024-08-23 14:51:19 +0530

    Day10

---
+++ Day10/string.js
@@ -0,0 +1,13 @@
+const str = "Mahipal Singh Jhala";
+
+// // Includes
+// console.log(str.includes('g'));
+
+// // Slice
+// console.log(str.slice(1,5));
+// console.log(str.slice(5,1));//returns empty string 
+
+// Substring
+
+ 
+
