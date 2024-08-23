2090008ad50dd654228e0abd50464454b6b96e26
mahipal <mahipalsinghjhala707@gmail.com>
2024-08-23 10:12:07 +0530

    day10

---
+++ Day10/task3.js
@@ -0,0 +1,19 @@
+const p = require('prompt-sync')();
+
+let x = parseInt(p("Enter the no. here : "));
+
+
+if (x==0) {
+    console.log("Zero");
+} 
+else if(x%2==0) {
+    console.log("even");
+}
+
+else if(x%2==1) {
+    console.log("odd");
+}
+else{
+    console.log("Invalid input :(");
+    
+}
