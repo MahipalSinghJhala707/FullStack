619059e08afff9286120d7fc04f4f2364a4c1e27
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2024-08-30 09:54:49 +0530

    react

--- R_E_A_C_T/REACT_VITE/FIRSTPJ/src/App.jsx
+++ R_E_A_C_T/REACT_VITE/FIRSTPJ/src/App.jsx
@@ -4,16 +4,20 @@ import './App.css'
 
 function App() {
   
-  let  [firstName,setFirstName] = useState('Mahipal')
-  function handle() {
-    setFirstName("Garv bhai !")
-  }
+  // let  [firstName,setFirstName] = useState('Mahipal')
+  // function handle() {
+  //   setFirstName("Garv bhai !")
+  // }
 
 
   return (
     <div>
-        <p>{firstName}</p>
-        <button onClick={handle}>Click me!</button>
+        {/* <p>{firstName}</p>
+        <button onClick={handle}>Click me!</button> */}
+        <form action="">
+          <label htmlFor="userName">Username</label>
+          <input type="text" name="userName" id="userName" />  
+        </form>
     </div>
   )
 }
