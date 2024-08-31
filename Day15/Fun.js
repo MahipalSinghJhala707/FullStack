cf63475be1fe37513bdb2e75c9268afc1b06ab7d
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2024-08-31 15:04:22 +0530

    Day15

---
+++ Day15/Fun.js
@@ -0,0 +1,12 @@
+function testFunctionForRest(...num1) {
+    return num1
+}
+
+const x = testFunctionForRest(100,200,300,400,500)
+console.log(x);
+function testFunctionForRest2(var1, var2 ,...num1) {
+    return num1
+}
+
+const y = testFunctionForRest2(100,200,300,400,500)
+console.log(y);
