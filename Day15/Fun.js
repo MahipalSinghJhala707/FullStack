ebfbc676c33c963b46f2922c58d3301e59cfd601
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2024-08-31 15:26:30 +0530

    Day15

--- Day15/Fun.js
+++ Day15/Fun.js
@@ -1,12 +1,32 @@
-function testFunctionForRest(...num1) {
-    return num1
+// function testFunctionForRest(...num1) {
+//     return num1
+// }
+
+// const x = testFunctionForRest(100,200,300,400,500)
+// console.log(x);
+// function testFunctionForRest2(var1, var2 ,...num1) {
+//     return num1
+// }
+
+// const y = testFunctionForRest2(100,200,300,400,500)
+// console.log(y);
+
+
+const object = {
+    userName : "Mahipal",
+    age : 20
 }
 
-const x = testFunctionForRest(100,200,300,400,500)
-console.log(x);
-function testFunctionForRest2(var1, var2 ,...num1) {
-    return num1
+// for(let key in object){
+//     console.log(key ,"|",object[key])
+// }
+function handleFunction(anyObject) {
+    console.log(`${anyObject.userName} is ${anyObject.age} years old !!!`);
 }
 
-const y = testFunctionForRest2(100,200,300,400,500)
-console.log(y);
+// handleFunction(object);
+handleFunction({
+    userName : "Kirtan",
+    age : 21
+});
+
