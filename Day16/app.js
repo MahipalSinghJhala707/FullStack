56be0a04a6d0608c09d04e38ca6f4a2cb2896e89
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2024-09-02 22:58:02 +0530

    Day18

---
+++ Day16/app.js
@@ -0,0 +1,13 @@
+// Data-types
+
+// # primitive 
+
+//types 7 : String , Number , Boolean , null , undefined , BigInt , Symbol
+
+// Reference (Non-primitive)
+// Arrays , Objects , Functions
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Stack (Primitive Data type) , Heap (NON-Primitive Data type)
