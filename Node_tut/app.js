aa560ea7ad93831790b98f89004493780cf2ea80
Mahipal Singh Jhala <mahipalsinghjhala707@gmail.com>
2024-09-10 00:09:20 +0530

    Day21

--- Node_tut/app.js
+++ Node_tut/app.js
@@ -2,19 +2,31 @@ const express = require('express');
 
 const app = express();
 
-app.get('/about', (req,res)=>{
-    res.send("yoyo honey singh")
-})
+// app.get('/about', (req,res)=>{
+//     res.send("yoyo honey singh")
+// })
 
-
-// Create server using express
-app.listen(3000,()=>{
-    console.log("Server is stared");
-    
-})
 
 const userData = [{username : 'abc',age : 22} , {username : 'xyz',age : 5}];
 
-app.post('/about' , (req,res)=>{
-    
+app.post('/about', (req, res) => {
+    // const newUser = req.body; // Get data sent in the request body
+  
+    // Add new user data to userData array
+  
+    // Respond with updated userData array
+    res.json({
+      message: "User data received successfully",
+      data: userData
+    });
+  });
+
+
+// Create server using express
+app.listen(
+    4000,
+    ()=>{
+        console.log("Server is stared");
+
 })
+
